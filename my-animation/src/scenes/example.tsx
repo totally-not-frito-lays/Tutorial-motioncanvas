af232afef0f46e2b3a8c56129288bf7f439519a2
Higgins-Codes <30853424+totally-not-frito-lays@users.noreply.github.com>
2023-11-28 23:31:12 -0800

    Added comments

--- my-animation/src/scenes/example.tsx
+++ my-animation/src/scenes/example.tsx
@@ -2,12 +2,13 @@ import {makeScene2D, Circle} from '@motion-canvas/2d';
 import {all, createRef} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
+  // Every object should have a reference
   const myCircle = createRef<Circle>();
 
+  // Default characteristics
   view.add(
     <Circle
       ref={myCircle}
-      // try changing these properties:
       x={-300}
       width={140}
       height={140}
@@ -15,8 +16,14 @@ export default makeScene2D(function* (view) {
     />,
   );
 
+  // Animation Logic: change one parameter at a time
+  yield* myCircle().fill('#e6a700', 1).to('#e13238', 1);
+
+  // Animation logic: change multiple parameters at the same time
   yield* all(
     myCircle().position.x(300, 1).to(-300, 1),
     myCircle().fill('#e6a700', 1).to('#e13238', 1),
   );
+
+  
 });
